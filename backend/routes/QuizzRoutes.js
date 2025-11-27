af29602668b7f3f4f9fdf7a760f28267054e7102
AravindKumar40m <aravindkumar40m@gmail.com>
2025-11-27 16:20:59 +0530

    backend commit

--- backend/routes/QuizzRoutes.js
+++ backend/routes/QuizzRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getQuizzes } from "../controllers/QuizzController.js";
 
+// quizz routes
+
 const router = express.Router();
 
 router.get("/quiz/:id", getQuizzes);
